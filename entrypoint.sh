f94824e19af539118e79e491a5a2bc5bf32ecd99
Adrian Cabala <a.cabala@exporo.com>
2020-12-17 11:50:02 +0100

    feat: create (empty) ssh file that is required for stack removal

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,15 +4,17 @@ echo "Stage $1"
 echo "Profile: $2"
 echo "Region: $3"
 echo "Command: $6"
+echo "Directory: $7"
 
 mkdir ~/.aws
 rm ~/.aws/credentials
 rm ~/.aws/config
 printf "[$2]\naws_access_key_id = $4\naws_secret_access_key = $5" >> ~/.aws/credentials
 printf "[$2]\nregion = $3" >> ~/.aws/config
-cat ~/.aws/config
-printf  "\n"
-cat ~/.aws/credentials
-printf "\n"
+# creating ssh file is required for `sls remove`
+mkdir ~/.ssh
+rm ~/.ssh/$2-$1
+touch  ~/.ssh/$2-$1
 
+cd $7
 serverless $6 --stage $1 --aws-profile $2
