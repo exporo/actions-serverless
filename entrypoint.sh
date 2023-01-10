7eef3779225c5b3891bd8c41cc149b0f1b5a2ccb
Miguel Vieira <m.vieira@exporo.com>
2023-01-10 19:37:10 +0000

    fix - again 12

--- entrypoint.sh
+++ entrypoint.sh
@@ -60,7 +60,7 @@ if [ $8 == 'true' ]; then
     #SLS_DEBUG=* serverless $4 --stage $1 --aws-profile $2
   fi
 else
-  if [ ! "$9" ]; then
+  if [ ! "$4" ]; then
     echo "XXXXX 5 -$9-"
     echo "size: -${#9}-"
     #serverless $4 --stage $1 --aws-profile $2  --data ${9@Q}
